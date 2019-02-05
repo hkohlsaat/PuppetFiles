2db6b81e07c740bc141ae2281d77e222057e311d
hkohlsaat <hkohlsaat@gmail.com>
2019-02-05 16:30:18 +0000

    Update vm2

--- Node/node-vm2.pp
+++ Node/node-vm2.pp
@@ -1,6 +1,6 @@
 class node_$vm2 {
         $admins = ["ad.min"]
-        $users = ["son.horst", "soehnchen.horst", "direx"]
+        $users = ["son.horst", "direx"]
 
         realize(Accounts::Virtual[$admins], Accounts::Sudoroot[$admins])
         realize(Accounts::Virtual[$users])
